5502bb7f8b0d4ba0393cebeb3035e7b0bc88966f
Hamoud Abass <hamoudabbas307@gmail.com>
2026-05-03 23:41:37 +0300

    Initial commit

---
+++ expenseTracker/db_models.py
@@ -0,0 +1,29 @@
+import sqlite3
+from typing import Any
+
+
+def create_db():
+
+    conn = sqlite3.connect("expense.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS expenses(
+        ID      INTEGER PRIMARY KEY AUTOINCREMENT,
+        Date    TEXT NOT NULL, 
+        Description TEXT NOT NULL,
+        Amount  REAL NOT NULL)
+        """)
+
+    conn.commit()
+    conn.close()
+
+def add(description:str, amount:int, date:str):
+    conn = sqlite3.connect("expense.db")
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO expenses(Date, Description, Amount) VALUES (?,?,?)",
+                (date,description,amount))
+    conn.commit()
+    conn.close()
+
---
+++ expenseTracker/main.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser,Namespace
+from db_models import create_db, add
+from datetime import date
+from views import display
+
+def main():
+    parser = ArgumentParser()
+    subparses = parser.add_subparsers(dest='command')
+
+    add_parser = subparses.add_parser("add", help="add a an expense")
+    add_parser.add_argument("--description", type=str, required= True)
+    add_parser.add_argument("--amount", type=float, required= True)
+
+    subparses.add_parser("view", help="view all expenses")
+
+    args: Namespace = parser.parse_args()
+
+    if args.command == "add":
+        create_db()
+        add(args.description, args.amount, date.today().isoformat())
+    elif args.command == 'view':
+        display()
+
+
+if __name__ == "__main__":
+    main()
