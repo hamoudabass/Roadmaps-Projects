f9b2113e7af26aef5d89d1e93b5ce2e086512c0d
Hamoud Abass <hamoudabbas307@gmail.com>
2026-05-04 22:35:35 +0300

    update main.py by adding delete,update,list,summary

--- expenseTracker/main.py
+++ expenseTracker/main.py
@@ -1,26 +1,52 @@
 from argparse import ArgumentParser,Namespace
-from db_models import create_db, add
+from db_models import create_db
+from controllers import add_expense, delete_expense, view_list, view_summary, view_summary_month, update_expense
 from datetime import date
-from views import display
 
 def main():
     parser = ArgumentParser()
     subparses = parser.add_subparsers(dest='command')
 
-    add_parser = subparses.add_parser("add", help="add a an expense")
+    add_parser = subparses.add_parser("add", help="add an expense")
     add_parser.add_argument("--description", type=str, required= True)
     add_parser.add_argument("--amount", type=float, required= True)
 
-    subparses.add_parser("view", help="view all expenses")
+    delete_parser = subparses.add_parser("delete", help="delete an expense")
+    delete_parser.add_argument("--id", type=int, required= True)
+
+    update_parser = subparses.add_parser("update", help="update an expense")
+    update_parser.add_argument("--id", type=int, required=True)
+    update_parser.add_argument("--description", type=str, required=True)
+    update_parser.add_argument("--amount", type=int, required=True)
+
+
+    subparses.add_parser("list", help="view all expenses")
+
+    summary_parser = subparses.add_parser("summary", help="view total expenses")
+    summary_parser.add_argument("--month", type=int, required=False)
 
     args: Namespace = parser.parse_args()
+    
+    create_db()
 
     if args.command == "add":
-        create_db()
-        add(args.description, args.amount, date.today().isoformat())
+        add_expense(args.description, args.amount, date.today().isoformat())
 
-    elif args.command == 'view':
-        display()
+    elif args.command == "update":
+        update_expense(args.id, args.description, args.amount)
+
+
+    elif args.command == "delete":
+        delete_expense(args.id)
+
+    elif args.command == 'list':
+        view_list()
+
+    elif args.command == 'summary':
+        if args.month:
+           view_summary_month()
+        else:
+            view_summary()
 
 
 if __name__ == "__main__":
